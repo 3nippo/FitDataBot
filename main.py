b954550eb8bf1241d11b2ec7fe3e3fbdfa9890ed
3nippo <mopsyaka13@gmail.com>
2023-07-24 01:29:55 +0300

    buttons

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 
 BOT_TOKEN = None
 with open('bot_token') as f:
@@ -11,5 +12,17 @@ BOT = telebot.TeleBot(BOT_TOKEN)
 def send_welcome(message):
     BOT.reply_to(message, "Howdy, how are you doing?")
 
+
+@BOT.message_handler(commands=['start'])
+@BOT.message_handler(func=lambda msg: True)
+def show_commands(message):
+    button_foo = types.InlineKeyboardButton('Foo', callback_data='foo')
+    button_bar = types.InlineKeyboardButton('Bar', callback_data='bar')
+
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(button_foo)
+    keyboard.add(button_bar)
+
+    BOT.reply_to(message, 'Select action:', reply_markup=keyboard)
 
 BOT.infinity_polling()
