5e4b350ab37025601b50aca85b09cfdcd32886d4
3nippo <mopsyaka13@gmail.com>
2023-07-24 01:17:41 +0300

    valid hello world

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import telebot
 
-BOT = None
+BOT_TOKEN = None
+with open('bot_token') as f:
+    BOT_TOKEN = f.read().strip()
+
+BOT = telebot.TeleBot(BOT_TOKEN)
+
 
 @BOT.message_handler(commands=['hello'])
 def send_welcome(message):
     BOT.reply_to(message, "Howdy, how are you doing?")
 
-def main():
-    bot_token = None
-    with open('bot_token') as f:
-        bot_token = f.read().strip()
 
-    BOT = telebot.TeleBot(bot_token)
-
-    BOT.infinity_polling()
-
-if __name__ == '__main__':
-    main()
+BOT.infinity_polling()
