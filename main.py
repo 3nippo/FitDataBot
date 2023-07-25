42d7d0b8f07b0e45472a8a8813142f1ad37caaed
3nippo <mopsyaka13@gmail.com>
2023-07-26 00:04:31 +0300

    async bot

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import telebot
 from telebot import types
+from telebot.async_telebot import AsyncTeleBot
+import asyncio
 
 BOT_TOKEN = None
 with open('bot_token') as f:
     BOT_TOKEN = f.read().strip()
 
-BOT = telebot.TeleBot(BOT_TOKEN)
+BOT = AsyncTeleBot(BOT_TOKEN)
 
 
 @BOT.message_handler(commands=['hello'])
-def send_welcome(message):
-    BOT.reply_to(message, "Howdy, how are you doing?")
+async def send_welcome(message):
+    await BOT.reply_to(message, "Howdy, how are you doing?")
 
 
 @BOT.message_handler(commands=['start'])
 @BOT.message_handler(func=lambda msg: True)
-def show_commands(message):
+async def show_commands(message):
     button_foo = types.InlineKeyboardButton('Foo', callback_data='foo')
     button_bar = types.InlineKeyboardButton('Bar', callback_data='bar')
 
@@ -23,6 +25,6 @@ def show_commands(message):
     keyboard.add(button_foo)
     keyboard.add(button_bar)
 
-    BOT.reply_to(message, 'Select action:', reply_markup=keyboard)
+    await BOT.reply_to(message, 'Select action:', reply_markup=keyboard)
 
-BOT.infinity_polling()
+asyncio.run(BOT.polling())
