52656cb5a737716da1c69f366beec80bec11b35d
3nippo <mopsyaka13@gmail.com>
2023-07-26 03:03:49 +0300

    complete timer

--- main.py
+++ main.py
@@ -11,25 +11,49 @@ with open('bot_token') as f:
 BOT = AsyncTeleBot(BOT_TOKEN)
 
 
-@BOT.message_handler(commands=['hello'])
+USER_CB = {}
+
+
+@BOT.message_handler(commands=['howdy'])
 async def send_welcome(message):
-    await BOT.reply_to(message, "Howdy, how are you doing?")
+    await BOT.reply_to(message, "Howdy, partner! 🤠")
 
-
-@BOT.message_handler(commands=['start'])
-@BOT.message_handler(func=lambda msg: True)
-async def show_commands(message):
-    button_foo = types.InlineKeyboardButton('Foo', callback_data='foo')
-    button_bar = types.InlineKeyboardButton('Bar', callback_data='bar')
-
-    keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(button_foo)
-    keyboard.add(button_bar)
-
-    await BOT.reply_to(message, 'Select action:', reply_markup=keyboard)
 
 @BOT.message_handler(commands=['timer'])
 async def timer(message):
-    await BOT.reply_to(message, message.text)
+    async def send_passed_time(ts):
+        await BOT.send_message(message.chat.id, "{:0>2}:{:0>2}".format(int(ts/60), int(ts % 60)))
 
-asyncio.run(BOT.polling())
+    timer = tools.AsyncTimer(send_passed_time, 3, 60)
+    def timer_stop():
+        timer.cancel()
+    
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    keyboard.add(telebot.types.KeyboardButton('Stop'))
+
+    await BOT.reply_to(message, 'Timer started!', reply_markup=keyboard)
+    USER_CB[message.from_user.id] = tools.TextCallback(timer_stop, 'Stop')
+    try:
+        await timer.start()
+    except asyncio.CancelledError:
+        pass
+    await BOT.send_message(message.chat.id, "Completed!", reply_markup=telebot.types.ReplyKeyboardRemove())
+
+
+@BOT.message_handler(func=lambda msg: True)
+async def process_message(message):
+    text_callback = USER_CB.get(message.from_user.id)
+    if text_callback and text_callback.call_back(message.text):
+        USER_CB.pop(message.from_user.id)
+
+
+async def main():
+    await BOT.set_my_commands([
+        telebot.types.BotCommand('timer', 'timer with suggestions'),
+        telebot.types.BotCommand('howdy', 'says howdy'),
+    ])
+
+    await BOT.polling()
+
+asyncio.run(main())
--- tools.py
+++ tools.py
@@ -1,18 +1,37 @@
 import asyncio
 
+
+class TextCallback:
+    def __init__(self, callback, cancellation_text):
+        self.callback = callback
+        self.text_to_cb = { cancellation_text: callback }
+
+    def call_back(self, text):
+        callback = self.text_to_cb.get(text)
+        if not callback:
+            return False
+        callback()
+        return True
+
+
 class AsyncTimer:
     def __init__(self, callback, every_sec, duration_sec):
         self.callback = callback
         self.every_sec = every_sec
         self.duration_sec = duration_sec
-        self.task = asyncio.ensure_future(self._job())
+        self.task = None
     
     async def _job(self):
         ts = 0
         while ts < self.duration_sec:
             await asyncio.sleep(self.every_sec)
-            await self.callback(ts)
             ts += self.every_sec
+            await self.callback(ts)
     
+    async def start(self):
+        if not self.task:
+            self.task = asyncio.ensure_future(self._job())
+        await asyncio.wait_for(self.task, None)
+
     def cancel(self):
         self.task.cancel()
